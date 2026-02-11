f5f3bf59f4fd8eeadfa2d8e306d5b73b8af24a8f
Vivek-Ravichandran05 <vivekravi.ram@gmail.com>
2026-02-11 11:42:37 +0530

    File updated Phase 1

--- Snowball-Bowling-Setup/src/main.js
+++ Snowball-Bowling-Setup/src/main.js
@@ -1,57 +1,116 @@
-import * as THREE from "three";
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import * as THREE from 'three';
 
-// Initializing the scene
+// -------------------- Scene --------------------
 
 const scene = new THREE.Scene()
+scene.background = new THREE.Color("#87CEEB")
 
-// ----------Ground(Bowling Lane)----------
+// -------------------- Ground(Bowling Lane) --------------------
 
-const GroundGeometry = new THREE.PlaneGeometry(6,40)
-const GroundMaterial = new THREE.MeshBasicMaterial({color:"#C19A6B"})
+const planeGeometry = new THREE.PlaneGeometry(6,40)
+const planeMaterial = new THREE.MeshStandardMaterial({color:"#C19A6B",side:THREE.DoubleSide})
 
-const Ground = new THREE.Mesh(GroundGeometry,GroundMaterial)
+const plane = new THREE.Mesh(planeGeometry,planeMaterial)
+plane.rotation.x = -Math.PI/2
+plane.position.y = -1
+plane.receiveShadow = true
+scene.add(plane)
 
-Ground.rotation.x = - Math.PI/2
-Ground.position.y = -1
+// -------------------- Bowling Snowball --------------------
 
-scene.add(Ground)
+const BallGeometry = new THREE.SphereGeometry(0.4,32,32)
+const BallMaterial = new THREE.MeshStandardMaterial({color:"white",
+    roughness:0.8,
+    metalness:0.1
+})
 
-// Initializing the object
+const Ball = new THREE.Mesh(BallGeometry,BallMaterial)
+Ball.position.set(0,-0.6,6)
+Ball.castShadow = true
+scene.add(Ball)
 
-const SphereGeometry = new THREE.SphereGeometry(0.4,32,32)
-const SphereMaterial = new THREE.MeshBasicMaterial({color:"white"})
+// -------------------- Lighting --------------------
 
-const SphereMesh = new THREE.Mesh(SphereGeometry,SphereMaterial)
-SphereMesh.position.z = 4
-scene.add(SphereMesh)
+// Ambient Light(Overall Lighting)
 
-// Initializing the camera
+const ambientlight = new THREE.AmbientLight(0xffffff,0.4)
+scene.add(ambientlight)
 
-const camera = new THREE.PerspectiveCamera(50,
-    window.innerWidth/window.innerHeight,
-    0.1,
-    50)
-camera.position.set(0,2,10)
-camera.lookAt(0, 0, -10)
+// Directional Light(main light source)
+
+const directionlight = new THREE.DirectionalLight(0xffffff,0.8)
+directionlight.position.set(5,10,5)
+directionlight.castShadow = true
+scene.add(directionlight)
+
+// -------------------- Bowling Pins --------------------
+
+const bodyGeometry = new THREE.CylinderGeometry(0.12,0.18,1,32)
+const bodyMaterial = new THREE.MeshStandardMaterial({color:0xffffff})
 
 
-// Initializing the render
+const headGeometry = new THREE.SphereGeometry(0.15,16,32)
+const headMaterial = new THREE.MeshStandardMaterial({color:"red"})
 
-const canvas = document.querySelector('canvas.threejs')
-const renderer = new THREE.WebGLRenderer({canvas : canvas})
+function CreatePin(x,z) {
+    const PinGroup = new THREE.Group()
+
+    const bodyMesh = new THREE.Mesh(bodyGeometry,bodyMaterial)
+    bodyMesh.position.y = 0.5
+    bodyMesh.castShadow = true
+
+    const headMesh = new THREE.Mesh(headGeometry,headMaterial)
+    headMesh.position.y = 1.1
+    headMesh.castShadow = true
+
+    PinGroup.add(bodyMesh,headMesh)
+    PinGroup.position.set(x,-1,z)
+    scene.add(PinGroup)
+}
+CreatePin(0,-12)
+
+CreatePin(-0.4,-13)
+CreatePin(0.4,-13)
+
+CreatePin(-0.8,-14)
+CreatePin(0,-14)
+CreatePin(0.8,-14)
+
+CreatePin(-1.2,-15)
+CreatePin(-0.4,-15)
+CreatePin(0.4,-15)
+CreatePin(1.2,-15)
+
+// -------------------- Camera --------------------
+
+const camera = new THREE.PerspectiveCamera(50,window.innerWidth/window.innerHeight,0.1,50)
+camera.position.set(0,1,10)
+camera.lookAt(0,0.5,-10)
+
+// -------------------- Renderer --------------------
+
+const canvas = document.querySelector("canvas.threejs")
+const renderer = new THREE.WebGLRenderer({canvas:canvas})
 renderer.setSize(window.innerWidth,window.innerHeight)
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-// Initializing the control
-const control = new OrbitControls(camera,canvas)
-control.enableDamping = true
-control.autoRotate = true
+// -------------------- Resizing Handling --------------------
 
-// Initializing the renderloop
-const renderloop = () =>{
-    control.update()
+window.addEventListener("resize",() => {
+    camera.aspect = window.innerWidth/window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth,window.innerHeight)
+})
+
+// -------------------- Animation Loop --------------------
+
+function animate(){
+    Ball.rotation.x -= 0.05; 
+    Ball.position.z -= 0.05;
+    
     renderer.render(scene,camera)
-    window.requestAnimationFrame(renderloop)
+    requestAnimationFrame(animate)
 }
 
-renderloop()
+animate()
